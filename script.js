c42f637c5dc610b44a58f090d98c388ca0e3ad26
JonnoChong <jonathanzjchong@gmail.com>
2022-06-26 22:16:38 +1000

    Added delete button

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ function inputLength(){
 
 //CREATES A LIST VALUE
 function createListElement(){
+    var div = document.createElement("div");
     var li = document.createElement("li");
-    // This var li has to go in the function, otherwise when we put new items in, it won't create a separate li for it
+    // This var li and div has to go in the function, otherwise when we put new items in, it won't create a separate li for it. 
     li.appendChild(document.createTextNode(input.value));
     //createTextNode is what we are appending to the li. Input we get from the variable above, and if we want the value, we just do .value
-    ul.appendChild(li);
+    div.appendChild(li);
+    ul.appendChild(div);
     input.value="";
+    li.addEventListener("click", crossListOff)
+    // This allows for new lines to be able to be crossed of the list
 }
 
 //FUNCTION TO ADD LIST AFTER CLICK
@@ -38,19 +42,29 @@ input.addEventListener("keypress", addListAfterKeyPress);
 
 
 
+// SETUP FOR TOGGLING ON AND OFF LIST ITEMS ALREADY IN HTML. FIRST 2 LINES CAN BE DELETED IF BLANK LIST
+var list = document.getElementsByTagName("li");
 
-
-// SET UP FOR TOGGLING ON AND OFF LIST ITEMS
-function toggle(){
-    var list = document.getElementsByTagName("li");
-
-    for (var i=0; i<list.length; i++) {
-        list[i].addEventListener("click", crossListOff);
-    }
-
-    function crossListOff(){
-        this.classList.toggle("done");
-    }
+for (var i=0; i<list.length; i++) {
+    list[i].addEventListener("click", crossListOff);
 }
 
-toggle();
+function crossListOff(){
+    this.classList.toggle("done");
+}
+
+
+
+//SETUP FOR DELETING THE LI IF DELETE BUTTON IS PRESSED 
+// var deleteBtn = document.getElementsByClassName("deleteBtn");
+
+// for (var i=0; i<list.length; i++) {
+//     deleteBtn[i].addEventListener("click", deleteAfterClick);
+// }
+
+// function deleteAfterClick(){
+
+// }
+
+
+
